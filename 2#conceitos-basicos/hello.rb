bf3c14472d13b93f6c41197683ae594c29747e57
Thales Augusto <thallles.ramos@gmail.com>
2019-07-02 15:15:09 -0300

    Aula 2 finalizada

---
+++ 2#conceitos-basicos/hello.rb
@@ -0,0 +1,13 @@
+# saida de dado
+print 'Digite seu nome: '
+
+# Recebendo uma entrada do teclado
+name = gets.chomp
+
+print 'Digite seu sobrenome: '
+
+sobrenome = gets.chomp
+
+# saida utilizando puts
+#use codigos ruby dentro de uma string com ${code}
+puts "Hello #{name} #{sobrenome}"
---
+++ 2#conceitos-basicos/missoes-especiais/missao-2/missao2.rb
@@ -0,0 +1,10 @@
+print 'Digite o primeiro número: '
+number1 = gets.chomp.to_i
+
+print 'Digite o segundo número: '
+number2 = gets.chomp.to_i
+
+puts "Adição: #{number1 + number2}"
+puts "Subtração: #{number1 - number2}"
+puts "Multiplicação: #{number1 * number2}"
+puts "Divisão: #{number1 / number2}"
